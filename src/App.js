2fc3ee95f80116e06d5dd3ba52fa750e9477c4d3
AG <knightauguest@gmail.com>
2021-08-04 14:11:27 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from "react";
+import HomePage from "./pages/home.page";
+import ProjectPage from "./pages/project.page";
+import NotFoundPage from "./pages/404";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>PM Demo</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/project" component={ProjectPage} />
+        <Route path="/notfound" component={NotFoundPage} />
+        <Redirect to="/notfound" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/project.page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ProjectPage = () => <h1>Project page</h1>;
+
+export default ProjectPage;
