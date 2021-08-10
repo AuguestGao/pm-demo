d07329ffd6539225ff440dd8674801899c2f5733
AG <knightauguest@gmail.com>
2021-08-10 19:08:54 -0400

    added redux, ducks pattern

---
+++ src/redux/slices/cardSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const cardsSlice = createSlice({
+  name: "cards",
+  initialState,
+  reducers: {
+    // reducers
+  },
+});
+
+export const {} = cardsSlice.actions;
+
+export default cardsSlice.reducer;
