1cbb6aee2db6d675081ee2a9f8867c75cf6fd2dc
AG <knightauguest@gmail.com>
2021-08-10 15:23:24 -0400

    added header

--- src/pages/project.page.jsx
+++ src/pages/project.page.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-const ProjectPage = ({ match }) => {
-  const { id } = match.params;
-  return <h1>Project {id} page</h1>;
-};
+const ProjectPage = () => <h1>Project page</h1>;
 
 export default ProjectPage;
